2bda35d2c47ee5e2681efa140cd1cb18aafbd606
TannerGilbert <gilberttanner.work@gmail.com>
2021-09-05 20:07:57 +0200

    added gelu derivative

--- Activation_Functions/gelu/code/gelu.py
+++ Activation_Functions/gelu/code/gelu.py
@@ -3,11 +3,12 @@ from scipy.special import erf
 
 
 class GELU:
-    def __call__(self, x: np.ndarray, approximate=True) -> np.ndarray:
+    def __call__(self, x: np.ndarray, approximate: bool = True) -> np.ndarray:
         if approximate:
-            return x / 2 * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3))))
-        return x / 2 * (1.0 + erf(x / np.sqrt(2.0)))
+            return x * 0.5 * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * np.power(x, 3))))
+        return x * 0.5 * (1.0 + erf(x / np.sqrt(2.0)))
 
-    # TODO: Add gradients
-    def gradient(self, x: np.ndarray) -> np.ndarray:
-        pass
+    def gradient(self, x: np.ndarray, approximate: bool = True) -> np.ndarray:
+        if approximate:
+            return 0.5 * np.tanh(0.0356774 * np.power(x, 3) + 0.797885 * x) + (0.0535161 * np.power(x, 3) + 0.398942 * x) * np.power(1 / np.cosh(x), 2) * (0.0356774 * np.power(x, 3) + 0.797885 * x) + 0.5
+        return 0.5 * (1.0 + erf(x / np.sqrt(2.0))) + x * 1 / np.sqrt(2 * np.pi) * np.exp(-0.5 * np.power(x, 2))
